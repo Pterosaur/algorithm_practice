07a731d717cdc05c6ea09662e7436e3e0167e7a5
Pterosaur <ganze718@gmail.com>
2017-08-24 00:16:54 +0800

    464. Can I Win

--- leetcode/464. Can I Win/464. Can I Win/464. Can I Win.cpp
+++ leetcode/464. Can I Win/464. Can I Win/464. Can I Win.cpp
@@ -33,18 +33,186 @@
 
 #include <vector>
 #include <algorithm>
+#include <cstdint>
+#include <unordered_map>
 
 using namespace std;
 
-class Solution {
+class candidate_set {
+private:
+    vector<bool> m_buffer;
 public:
-    bool canIWin(int maxChoosableInteger, int desiredTotal) {
-        
+    candidate_set(size_t total) {
+        m_buffer.resize(total + 1, true);
+    }
+    bool get(size_t index) const {
+        return m_buffer[index];
+    }
+    void set(size_t index, bool value) {
+        if (index < m_buffer.size()) {
+            m_buffer[index] = value;
+        }
+    }
+    size_t size() const {
+        return m_buffer.size();
+    }
+
+    bool operator == (const candidate_set & cs) const{
+        return m_buffer == cs.m_buffer;
+    }
+    size_t hash() const {
+        return std::hash<vector<bool>>()(m_buffer);
     }
 };
 
+struct candidate_set_hash {
+    size_t operator()(const candidate_set & cs) const {
+        return cs.hash();
+    }
+};
+
+
+class Solution {
+private:
+    unordered_map<candidate_set, bool, candidate_set_hash> m_record;
+    enum Round {
+        My,
+        Opponent,
+    };
+
+    bool canIWin(candidate_set & choosable_set, int desiredTotal, Round rd) {
+        auto pre_result = m_record.find(choosable_set);
+        if (pre_result != m_record.end()) {
+            return pre_result->second;
+        }
+        bool result = false;
+        if (rd == My) {
+            for (int i = 1; i < choosable_set.size(); i++) {
+                if (choosable_set.get(i)) {
+                    choosable_set.set(i, false);
+                    if ((desiredTotal - i) <= 0 || canIWin(choosable_set, desiredTotal - i, Opponent)) {
+                        choosable_set.set(i, true);
+                        result = true;
+                        goto OUTPUT;
+                    }
+                    choosable_set.set(i, true);
+                }
+            }
+            result = false;
+        }
+        else {
+            for (int i = 1; i < choosable_set.size(); i++) {
+                if (choosable_set.get(i)) {
+                    choosable_set.set(i, false);
+                    if ((desiredTotal - i) <= 0 || !canIWin(choosable_set, desiredTotal - i, My)) {
+                        choosable_set.set(i, true);
+                        result = false;
+                        goto OUTPUT;
+                    }
+                    choosable_set.set(i, true);
+                }
+            }
+            result = true;
+        }
+
+OUTPUT:
+        m_record[choosable_set] = result;
+        return result;
+    }
+
+public:
+    bool canIWin(int maxChoosableInteger, int desiredTotal) {
+        if (maxChoosableInteger >= desiredTotal) {
+            return true;
+        }
+        else if (((1 + maxChoosableInteger)*maxChoosableInteger) / 2 < desiredTotal) {
+            return false;
+        }
+        candidate_set choosable_set(maxChoosableInteger);
+        return canIWin(choosable_set, desiredTotal, My);
+
+    }
+};
+
+
+
+
+class Solution_int {
+private:
+    unordered_map<size_t, bool> m_record;
+    int m_total;
+    enum Round {
+        My,
+        Opponent,
+    };
+
+    bool canIWin(size_t choosable_set, int desiredTotal, Round rd) {
+        auto pre_result = m_record.find(choosable_set);
+        if (pre_result != m_record.end()) {
+            return pre_result->second;
+        }
+        bool result = false;
+        if (rd == My) {
+            for (int i = 1; i <= m_total; i++) {
+                if (choosable_set & ((size_t)1<<i)) {
+                    size_t next_choosable = choosable_set;
+                    next_choosable &= ~((size_t)1 << i);
+                    if ((desiredTotal - i) <= 0 || canIWin(next_choosable, desiredTotal - i, Opponent)) {
+                        result = true;
+                        goto OUTPUT;
+                    }
+                }
+            }
+            result = false;
+        }
+        else {
+            for (int i = 1; i <= m_total; i++) {
+                if (choosable_set & ((size_t)1<<i)) {
+                    size_t next_choosable = choosable_set;
+                    next_choosable &= ~((size_t)1 << i);
+                    if ((desiredTotal - i) <= 0 || !canIWin(next_choosable, desiredTotal - i, My)) {
+                        result = false;
+                        goto OUTPUT;
+                    }
+                }
+            }
+            result = true;
+        }
+
+OUTPUT:
+        m_record[choosable_set] = result;
+        return result;
+    }
+
+public:
+    bool canIWin(int maxChoosableInteger, int desiredTotal) {
+        if (maxChoosableInteger >= desiredTotal) {
+            return true;
+        }
+        else if (((1 + maxChoosableInteger)*maxChoosableInteger) / 2 < desiredTotal) {
+            return false;
+        }
+        m_total = maxChoosableInteger;
+        size_t choosable_set = 0;
+        for (int i = 1; i <= m_total; i++) {
+            choosable_set |= ((size_t)1 << i);
+        }
+        return canIWin(choosable_set, desiredTotal, My);
+
+    }
+};
+
+
+
+
+#include <iostream>
 int main()
 {
+    Solution_int s;
+    //std::cout <<  s.canIWin(20, 200) << std::endl;
+    bool result = s.canIWin(10, 11);
     return 0;
 }
+
+
 
